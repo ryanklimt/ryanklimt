ed9a526e3a79d66a0667d6dfd02ca2de7053f85b
Ryan Klimt <ryanklimt@gmail.com>
2014-07-26 17:47:49 -0500

    Some fixes for localhost db connections

--- includes/database.config.php
+++ includes/database.config.php
@@ -1,9 +1,16 @@
 <?php
 
-	$Database['Host'] = 'mysql13.000webhost.com';
-	$Database['Name'] = 'a3544935_rklimt';
-	$Database['User'] = 'a3544935_rklimt';
-	$Database['Pass'] = 'Ryanjohn64';
+	if(stripos($_SERVER['SERVER_NAME'], 'rklimt.com') !== FALSE) {
+		$Database['Host'] = 'mysql13.000webhost.com';
+		$Database['Name'] = 'a3544935_rklimt';
+		$Database['User'] = 'a3544935_rklimt';
+		$Database['Pass'] = 'Ryanjohn64';
+	} else {
+		$Database['Host'] = 'localhost';
+		$Database['Name'] = 'test';
+		$Database['User'] = 'root';
+		$Database['Pass'] = 'mysql';
+	}
 
 	if (!defined('DB_HOST')) {
 		define('DB_HOST', $Database['Host']);
