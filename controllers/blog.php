ac2db68f32865023d852e7f9bb9636594a0ebced
Ryan Klimt <ryanklimt@gmail.com>
2014-07-26 22:53:55 -0500

    Fix numPages

--- controllers/blog.php
+++ controllers/blog.php
@@ -1,7 +1,8 @@
 <?php
 	$page = isset($_params[0]) && is_numeric($_params[0]) ? $_params[0] : 1;
 	$perPage = 3;
-	$numPages = (int)(numBlogPosts() / $perPage);
+	$numPages = numBlogPosts() / $perPage;
+	if(strpos($numPages,".")) $numPages = floor($numPages) + 1;
 
 	if($page < 1) $page = 1;
 	if($page > $numPages) $page = $numPages;
