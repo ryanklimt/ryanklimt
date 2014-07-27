6f7e0b08f3c3a094948bf1f1d012e0d94bd4cf2a
Ryan Klimt <ryanklimt@gmail.com>
2014-07-26 19:11:46 -0500

    Add seo

--- includes/@seo.php
+++ includes/@seo.php
@@ -3,6 +3,7 @@
 		"home" => "Web Development",
 		"about" => "Who I Am",
 		"work" => "My Work",
+		"blog" => "My Thoughts",
 		"resume" => "My Accomplishments",
 		"contact" => "Contact",
 		"404" => "404 Error",
