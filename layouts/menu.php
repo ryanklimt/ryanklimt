9e9fdb85b6710e75a1709de2df9a1927468c59aa
Ryan Klimt <ryanklimt@gmail.com>
2014-07-26 19:07:32 -0500

    Add about in menu

--- layouts/menu.php
+++ layouts/menu.php
@@ -3,7 +3,7 @@
 		<li <?php echo is_current_page('home', false); ?>><a href=''>Home</a></li>
 		<li <?php echo is_current_page('about', false); ?>><a href='about'>About</a></li>
 		<li <?php echo is_current_page('work', false); ?>><a href='work'>Work</a></li>
-		<!--<li <?php echo is_current_page('blog', false); ?>><a href='blog'>Blog</a></li>-->
+		<li <?php echo is_current_page('blog', false); ?>><a href='blog'>Blog</a></li>
 		<li <?php echo is_current_page('resume', false); ?>><a href='resume'>Resume</a></li>
 		<li <?php echo is_current_page('contact', false); ?>><a href='contact'>Contact</a></li>
 	</ul>
