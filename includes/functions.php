b3dcbd38125ed05bc5b7076b19794411492f9146
Ryan Klimt <ryanklimt@gmail.com>
2014-07-29 17:11:12 -0500

    Add function for query

--- includes/functions.php
+++ includes/functions.php
@@ -8,17 +8,19 @@
 		$date = $x[2][0] == "0" ? $x[2][1] : $x[2];
 		return $date . ' ' . $m . ' ' . $x[0];
 	}
-	function numBlogPosts() {
+	function query($query) {
 		$db = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-		$query = "SELECT id FROM blog";
-		$result = $db->query($query);
+		$results = $db->query($query);
+		mysqli_close($db);
+		return $results;
+	}
+	function numBlogPosts() {
+		$result = query("SELECT id FROM blog");
 		return $result->num_rows;
 	}
 	function getBlogPosts($page, $perPage) {
 		$start = ($page * $perPage) - $perPage;
-		$db = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-		$query = "SELECT * FROM blog LIMIT ".$start.", ".$perPage;
-		$result = $db->query($query);
+		$result = query("SELECT * FROM blog LIMIT ".$start.", ".$perPage);
 		$posts = array();
 		if($result->num_rows > 0) {
 			$i = 0;
@@ -27,7 +29,6 @@
 				$i++;
 			}
 		}
-		mysqli_close($db);
 		return $posts;
 	}
 ?>
