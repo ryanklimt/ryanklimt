4abc1eff6a4b78f1d16e18981a65956e842a036a
Ryan Klimt <ryanklimt@gmail.com>
2015-05-16 13:59:50 -0400

    Change to pulling in wallpapers from reddit

--- process/randompicture.php
+++ process/randompicture.php
@@ -1,9 +1,16 @@
 <?php
 	date_default_timezone_set('America/Indianapolis');
-	$response = json_decode(file_get_contents('https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=4d564a554e2a415340c51e631150df8a&per_page=35&extras=url_o&format=json&nojsoncallback=1'));
-	$photos_array = $response->photos->photo;
-	$single_photo = $photos_array[date('d')];
-	$photo_url = 'https://farm'.$single_photo->farm.'.staticflickr.com/'.$single_photo->server.'/'.$single_photo->id.'_'.$single_photo->secret.'_b.jpg';
+	$allLinks = array();
+	$xml = simplexml_load_string(file_get_contents('http://www.reddit.com/r/wallpapers/top/.xml'));
+	foreach($xml->channel->item as $images) {
+		$DOM = new DOMDocument;
+		$DOM->loadHTML($images->description);
+		$link = $DOM->getElementsByTagName('a')->item(2)->getAttribute('href');
+		if(substr($link, -3) == 'jpg') {
+			array_push($allLinks, $link);
+		}
+	}
+	$chosenPhoto = $allLinks[date('d')%sizeof($allLinks)];
 	header('Content-type: image/jpeg');
-	readfile($photo_url);
+	readfile($chosenPhoto);
 ?>
