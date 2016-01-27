f13bbf72c35e39ba03091fd485e31583e4e2d789
Ryan Klimt <ryanklimt@gmail.com>
2016-01-27 15:21:56 -0500

    Update randompicture.php

--- process/randompicture.php
+++ process/randompicture.php
@@ -1,17 +1,17 @@
 <?php
 	date_default_timezone_set('America/Indianapolis');
 	$allLinks = array();
-	$xml = simplexml_load_string(file_get_contents('http://www.reddit.com/r/wallpapers/top/.xml'));
-	foreach($xml->channel->item as $images) {
-		$DOM = new DOMDocument;
-		$DOM->loadHTML($images->description);
-		$link = $DOM->getElementsByTagName('a')->item(2)->getAttribute('href');
-		if(substr($link, -3) == 'jpg') {
-			array_push($allLinks, $link);
-		}
+	$file = file_get_contents('https://www.reddit.com/r/wallpapers/top/.xml');
+	$arr = preg_split('/https?:\/\/imgur.com\//', htmlspecialchars($file));
+	$count = 0;
+	foreach($arr as $str) {
+	    if($count > 0) {
+	        array_push($allLinks, "http://imgur.com/".substr($arr[$count], 0, 7).".jpg<br>");
+	    }
+	    $count++;
 	}
 	$chosenPhoto = $allLinks[date('d')%sizeof($allLinks)];
-	header('Cache-Control: max-age=3600');
+	header('Cache-Control: max-age=1800');
 	header('Content-type: image/jpeg');
 	readfile($chosenPhoto);
 ?>
