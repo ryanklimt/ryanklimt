14d2211ef9506a7c9fb994f577929289bb7b5429
Ryan Klimt <ryanklimt@gmail.com>
2016-06-30 09:21:31 -0400

    Fix random picture (broken)

--- process/randompicture.php
+++ process/randompicture.php
@@ -1,18 +1,12 @@
 <?php
 	date_default_timezone_set('America/Indianapolis');
 	$allLinks = array();
-	$file = file_get_contents('https://www.reddit.com/r/wallpapers/top/.xml');
-	$arr = preg_split('/https?:\/\/imgur.com\//', htmlspecialchars($file));
-	$count = 0;
-	foreach($arr as $str) {
-	    $link = substr($arr[$count], 0, 7);
-	    if($count > 0 && !strpos($link, '/')) {
-	        array_push($allLinks, "http://imgur.com/".$link.".jpg<br>");
-	    }
-	    $count++;
+	foreach(simplexml_load_string(file_get_contents('https://www.reddit.com/r/wallpapers/top/.xml')) as $k) {
+		if(preg_match_all("/<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>/siU", $k->content, $matches) && (strpos($matches[2][2], '.jpg') || strpos($matches[2][2], '.jpeg') || strpos($matches[2][2], '.png'))) {
+			array_push($allLinks, $matches[2][2]);
+		}
 	}
-	$chosenPhoto = $allLinks[date('d')%sizeof($allLinks)];
 	header('Cache-Control: max-age=1800');
 	header('Content-type: image/jpeg');
-	readfile($chosenPhoto);
+	readfile($allLinks[date('d')%sizeof($allLinks)]);
 ?>
