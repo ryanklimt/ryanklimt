b40e2d76738d55a4e471d4a4f4a6c078576b0040
Ryan Klimt <ryanklimt@gmail.com>
2016-06-30 09:47:36 -0400

    Update randompicture.php

--- process/randompicture.php
+++ process/randompicture.php
@@ -1,7 +1,7 @@
 <?php
 	$allLinks = array();
 	foreach(simplexml_load_string(file_get_contents('https://www.reddit.com/r/EarthPorn/top/.xml')) as $k) {
-		if(preg_match_all("/<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>/siU", $k->content, $matches) && (strpos($matches[2][2], '.jpg') || strpos($matches[2][2], '.jpeg') || strpos($matches[2][2], '.png'))) {
+		if(preg_match_all('/<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>/siU', $k->content, $matches) && (strpos($matches[2][2], '.jpg') || strpos($matches[2][2], '.jpeg') || strpos($matches[2][2], '.png'))) {
 			array_push($allLinks, $matches[2][2]);
 		}
 	}
