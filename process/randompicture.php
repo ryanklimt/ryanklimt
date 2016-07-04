444caa9ce46bec2aa0e2da3a30c09385deff78b1
Ryan Klimt <ryanklimt@gmail.com>
2016-07-03 22:42:53 -0400

    Update randompicture.php

--- process/randompicture.php
+++ process/randompicture.php
@@ -7,7 +7,25 @@
 	}
 	header('Cache-Control: max-age=1800');
 	if(isset($_GET['rss'])) {
-		header('Content-Type: application/rss+xml; charset=ISO-8859-1');
+	    header('Content-Type: application/xml');
+        $rssfeed = '<?xml version="1.0" encoding="UTF-8" ?>';
+        $rssfeed .= '<rss version="2.0">';
+        $rssfeed .= '<channel>';
+        $rssfeed .= '<title>Ryan Klimt Random Picture RSS Feed</title>';
+        $rssfeed .= '<link>https://theklimts.com</link>';
+        $rssfeed .= '<description>RSS feed generated from Reddit EarthPorn top wallpapers.</description>';
+        $rssfeed .= '<language>en-us</language>';
+        $rssfeed .= '<copyright>Copyright (C) '.date('Y').' Ryan Klimt</copyright>';
+        foreach($allLinks as $link) {
+            $rssfeed .= '<item>';
+            $rssfeed .= '<title>' . $link . '</title>';
+            $rssfeed .= '<link>' . $link . '</link>';
+            $rssfeed .= '<description><img src="' . $link . '" /></description>';
+            $rssfeed .= '</item>';
+        }
+        $rssfeed .= '</channel>';
+        $rssfeed .= '</rss>';
+        echo $rssfeed;
 	} else {
 		header('Content-type: image/jpeg');
 		readfile($allLinks[date('d')%sizeof($allLinks)]);
